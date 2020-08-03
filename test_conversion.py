53db8a6e83d8ce5c733c8465de4e24792651e274
HPCClass Admin <train37@comet-ln2.sdsc.edu>
2020-08-03 12:16:10 -0700

    fixed extra digits

--- test_conversion.py
+++ test_conversion.py
@@ -4,7 +4,7 @@ assert conversion.dollars2cents(1) == 100
 assert conversion.dollars2cents(.1) == 10
 assert conversion.dollars2cents(0) == 0
 
-assert conversion.gallons2liters(1) == 3.785
+assert conversion.gallons2liters(1) == 3.78541
 assert conversion.gallons2liters(2) == 7.57082
 assert conversion.gallons2liters(0) == 0
 
